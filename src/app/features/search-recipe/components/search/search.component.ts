202005bc718ee388a5440682afe63c9a728ed8b5
inan <inanygt@hotmail.com>
2024-09-26 09:20:24 +0200

    get user logged in state

--- src/app/features/search-recipe/components/search/search.component.ts
+++ src/app/features/search-recipe/components/search/search.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { IngredientInterface } from '../../../../core/types/ingredient.interface';
+import { AuthService } from '../../../../core/components/authentication/service/auth.service';
 
 @Component({
    selector: 'app-search',
    templateUrl: './search.component.html',
    styleUrl: './search.component.scss'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
    title = 'leftOver';
+
+   constructor(
+      private authService: AuthService
+   ) { }
+
+   ngOnInit(): void {
+
+   }
 }
